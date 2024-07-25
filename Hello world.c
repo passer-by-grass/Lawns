33579077dd0a287d33f287322c7087b2186b3fd2
passer-by-grass <3415777952@qq.com>
2024-07-25 11:18:45 +0800

    first commit

---
+++ Hello world.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	
+	return 0;
+}
+//一、程序框架：
+//#include<stdio.h>
+//int main() 
+//{
+//    这一行写代码 ;
+//    return 0;
+//}
+
+//二、输出
+//printf('hello world!\n");
+//""里面的东西叫做字符串，printf会把里面的东西原封不动地输出
+//\n表示需要在输出结果后面换一行 
